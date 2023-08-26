00b9b2cc23d9bad265c93311daf9b7ed49278a30
Mukul R <mukulgrao@gmail.com>
2023-08-25 19:58:56 -0700

    Updated add review api to update instead of adding new rows

--- API/addreview.php
+++ API/addreview.php
@@ -33,30 +33,53 @@ if($connection) {
 	if (strcmp(trim($headers["api-key"]), trim($apikey)) == 0) {
 		if (array_key_exists("deviceID", $data) && array_key_exists("foodID", $data) && array_key_exists("foodName", $data) && array_key_exists("rating", $data)) {
 
-			$insertQuery = "INSERT INTO $tablename (`deviceID`, `foodID`, `foodName`, `rating`, `unixtime`) VALUES (?, ?, ?, ?, ?)";
-			$stmt = mysqli_prepare($connection, $insertQuery);
-			if ($stmt) {
-				mysqli_stmt_bind_param($stmt, "sisii", $data['deviceID'], $data['foodID'], $data["foodName"], $data['rating'], time());
-				if (mysqli_stmt_execute($stmt)) {
+			$checkQuery = "SELECT * FROM $tablename WHERE `deviceID` = ? AND `foodID` = ?";
+			$selectStmt = mysqli_prepare($connection, $checkQuery);
+			mysqli_stmt_bind_param($selectStmt, "si", $data['deviceID'], $data['foodID']);
+			mysqli_stmt_execute($selectStmt);
+			$result = mysqli_stmt_get_result($selectStmt);
+
+			if (mysqli_num_rows($result) > 0) {
+				// Row already exists, perform an update
+				$updateQuery = "UPDATE $tablename SET `rating` = ?, `unixtime` = ? WHERE `deviceID` = ? AND `foodID` = ?";
+				$updateStmt = mysqli_prepare($connection, $updateQuery);
+				mysqli_stmt_bind_param($updateStmt, "iisi", $data['rating'], time(), $data['deviceID'], $data['foodID']);
+
+				if (mysqli_stmt_execute($updateStmt)) {
 					header('Content-Type: application/json');
 					echo json_encode(array(
 						"status" => "success",
 					));
 				} else {
-					echo("fail");
 					http_response_code(400);
 				}
-				mysqli_stmt_close($stmt);
+
+				mysqli_stmt_close($updateStmt);
 			} else {
-				echo("failst");
-				http_response_code(400);
+				// Row doesn't exist, perform an insert
+				$insertQuery = "INSERT INTO $tablename (`deviceID`, `foodID`, `foodName`, `rating`, `unixtime`) VALUES (?, ?, ?, ?, ?)";
+				$insertStmt = mysqli_prepare($connection, $insertQuery);
+				mysqli_stmt_bind_param($insertStmt, "sisii", $data['deviceID'], $data['foodID'], $data["foodName"], $data['rating'], time());
+
+				if (mysqli_stmt_execute($insertStmt)) {
+					header('Content-Type: application/json');
+					echo json_encode(array(
+						"status" => "success",
+					));
+				} else {
+					http_response_code(400);
+				}
+
+				mysqli_stmt_close($insertStmt);
 			}
+
+			mysqli_stmt_close($selectStmt);
+
 		} else {
-			echo("fail2");
 			http_response_code(400);
 		}
 	} else {
-		echo("fail3");
+		echo("Unauthorized.");
 		http_response_code(401);
 	}
 } else {
